75910f954de92eb26a6e6ed7e1f063f5bd60b91f
zoodogood <zoodogood@gmail.com>
2023-08-14 18:47:53 +0300

    Clean a code

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ import { collection, handlePoints } from '@/api';
 handlePoints(app, [...collection.values()]);
 
 
-if (!parseInt(env.IS_ON_VERSEL_PRODUCTION!)){
+if (!parseInt(env.IS_ON_VERSEL_PRODUCTION ?? "0")){
 	const {server} = await listenExpress({port: 8_000});
 	console.log(server.address());
 }
