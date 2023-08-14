3fef441b720f8cee9a8bd6c795a85d68902284bb
zoodogood <zoodogood@gmail.com>
2023-08-14 10:08:56 +0300

    Fix for versel production

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,17 @@
 
-import { listen as listenExpress } from '@/Express';
+import { listen as listenExpress, app } from '@/Express';
+import { env } from '@root/lib/IUsingProcessEnv';
 
-const {server, app} = await listenExpress({port: 8_000});
 
 import { collection, handlePoints } from '@/api';
 
 handlePoints(app, [...collection.values()]);
 
-console.log(server.address());
+
+if (!parseInt(env.IS_ON_VERSEL_PRODUCTION!)){
+	const {server} = await listenExpress({port: 8_000});
+	console.log(server.address());
+}
 
 
+export default app;
