6801a2e69d80bf919d4ffe99485fd0cb35fb2549
zoodogood <zoodogood@gmail.com>
2023-08-14 10:06:50 +0300

    Init page: /zoodogood

---
+++ client/src/views/ZoodogoodPage.vue
@@ -0,0 +1,41 @@
+<template>
+жмяк
+<input type="text">
+<img src="@/assets/raccoon.png" alt="">
+</template>
+
+<script lang="ts" setup>
+
+
+</script>
+
+<style>
+body 
+{
+  background-color: #faebd7;
+  font-family: sans-serif;
+  font-size: 2em;
+}
+
+input
+{
+  background-color: #7fffd405;
+  border-color: #7fffd422;
+  border-radius: 30px;
+  outline: none;
+  padding: 30px;
+
+  background-color: #2cd39b26;
+
+}
+</style>
+
+<style scoped>
+  img
+  {
+    width: calc(15vw + 100px);
+    position: fixed;
+    left: calc(3vw + 20px);
+    bottom: calc(3vw + 20px);
+  }
+</style>
