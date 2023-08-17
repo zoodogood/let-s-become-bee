f31d7a65258ee6b6ab28f7683f9a0cc6a20f9f7c
zoodogood <zoodogood@gmail.com>
2023-08-17 14:13:27 +0300

    Fix: correct function overloads

--- server/src/api/interfaces.ts
+++ server/src/api/interfaces.ts
@@ -1,16 +1,21 @@
 import type { Request, Response } from "express";
 
 
-type PointCallback = (request: Request, response: Response, next?: () => unknown) => unknown;
-type PointCallbackWithErrorParam = (error: Error, request: Request, response: Response, next?: () => unknown) => unknown;
+interface PointCallback {
+	(request: Request, response: Response, next?: () => unknown): unknown;
+	(error: Error, request: Request, response: Response, next?: () => unknown): unknown;
+}
+
 
 interface IApiPointModule {
 	path: string,
 	name: string,
 	priorityIndex?: number,
-	get?: PointCallback | PointCallbackWithErrorParam,
-	post?: PointCallback | PointCallbackWithErrorParam,
-	use?: PointCallback | PointCallbackWithErrorParam
+	get?: PointCallback,
+	post?: PointCallback,
+	use?: PointCallback
 }
+
+
 
 export { IApiPointModule };
