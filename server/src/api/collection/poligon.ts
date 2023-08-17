76e97129abd729e37eab72a56373784d90e601ad
zoodogood <zoodogood@gmail.com>
2023-08-17 12:11:16 +0300

    Working on a api point; Add feature: handle 404

--- server/src/api/collection/poligon.ts
+++ server/src/api/collection/poligon.ts
@@ -2,21 +2,30 @@
 import type { Request, Response } from "express";
 import { IApiPointModule } from "@/api/interfaces";
 
-function get(request: Request, response: Response){
-	response.send("job");
-	return;
-}
+async function use(request: Request, response: Response){
+	const isPublic = await (async () => {
+		const PUBLIC_NAME = "public";
+		const {collection} = await import('@/api');
+		const path = collection.get(PUBLIC_NAME)?.path;
+		if (!path){
+			return null;
+		}
+		console.dir({path, public: request}, {showHidden: true, depth: 1});
+		
+		
 
-function use(request: Request, response: Response){
-	console.info(request.path);
-	response.send("hi is 404 testing");
+		// to-do: слабое место, плохая, ненаёжная проверка; используется локальная константа PUBLIC_NAME вместо внешней привязки
+		return `/${ request.originalUrl }`.startsWith(path);
+	})();
+
+	const message = isPublic ? "is public 404" : "is 404";
+	response.send(message);
 }
 
 
 export default {
 	name: "poligon",
-	path: "/poligon",
+	path: "*",
 	priorityIndex: -1,
-	get,
 	use
 } as IApiPointModule;
