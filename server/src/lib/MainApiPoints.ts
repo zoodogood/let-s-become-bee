585bcd3a7d15386aed71c76027fa536ba1ba9c58
zoodogood <zoodogood@gmail.com>
2023-08-17 14:13:48 +0300

    Add MainApiPoints enum

---
+++ server/src/lib/MainApiPoints.ts
@@ -0,0 +1,8 @@
+const enum MainApiPointsNameEnum {
+	Static = "public",
+	ClientIndex = "public/index",
+	NotFound = "404",
+	Index = "index" // Return index.html
+}
+
+export { MainApiPointsNameEnum };
