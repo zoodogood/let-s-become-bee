1010b5de215c1ecda0f7bd55b2d101a29a547e99
zoodogood <zoodogood@gmail.com>
2023-08-17 14:20:00 +0300

    Add api point /public/index

--- server/src/api/collection/mod.ts
+++ server/src/api/collection/mod.ts
@@ -3,3 +3,4 @@ export {default as $2} from './index';
 export {default as $3} from './404';
 export {default as $4} from './smile';
 export {default as $5} from './public';
+export {default as $6} from './publicIndex';
