ab187a8b93c2e6f4749f861aae9a2e625395719b
zoodogood <zoodogood@gmail.com>
2023-08-17 08:00:33 +0300

    Fix sort function

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,12 +1,13 @@
 import * as collectionRaw from '@/api/collection/mod';
 import { Collection } from '@discordjs/collection';
 
-function compareByPriorityIndex(a: {priorityIndex?: number}, b: {priorityIndex?: number}): number {
-	return Number((a.priorityIndex ?? 0) < (b.priorityIndex ?? 0));
+function compareByPriorityIndexForSort(a: {priorityIndex?: number}, b: {priorityIndex?: number}) {
+	const isBigThan = (a.priorityIndex ?? 0) > (b.priorityIndex ?? 0);
+	return (-1) ** +isBigThan;
 }
 
 const collection = new Collection(Object.entries(collectionRaw))
-	.sort(compareByPriorityIndex);
+	.sort(compareByPriorityIndexForSort);
 
 export { handlePoints, handlePoint } from "@/api/handleApiPoints";
 export { collection };
