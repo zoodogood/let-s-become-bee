b0a3fd4b588fb981aa5ec93b0fc32a7202055384
Yakyzzza163 <ksport124@gmail.com>
2023-08-17 13:35:36 +0700

    Clean a code

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,12 +1,3 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	
 </template>
-
-<script lang="ts" setup>
-
-import HelloWorld from '@/components/HelloWorld.vue';
-
-</script>
