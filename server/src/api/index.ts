516603d5c14b924bcfab3c0a979ade34daa5111c
zoodogood <zoodogood@gmail.com>
2023-08-17 14:12:45 +0300

    Refact; Add feature: collection keys are names
    Previously keys are factual raw module name

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,12 +1,16 @@
 import * as collectionRaw from '@/api/collection/mod';
 import { Collection } from '@discordjs/collection';
+import type { IApiPointModule } from '@/api/interfaces';
 
 function compareByPriorityIndexForSort(a: {priorityIndex?: number}, b: {priorityIndex?: number}) {
 	const isBigThan = (a.priorityIndex ?? 0) > (b.priorityIndex ?? 0);
 	return (-1) ** +isBigThan;
 }
 
-const collection = new Collection(Object.entries(collectionRaw))
+const apiPointEntries = Object.values(collectionRaw)
+	.map(point => [point.name, point]) as [string, IApiPointModule][];
+
+const collection = new Collection(apiPointEntries)
 	.sort(compareByPriorityIndexForSort);
 
 export { handlePoints, handlePoint } from "@/api/handleApiPoints";
