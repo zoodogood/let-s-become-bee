213458ce47f97ae69642b6128c4d92926292ae1a
zoodogood <zoodogood@gmail.com>
2023-08-17 12:12:21 +0300

    Add api point ./public

--- server/src/api/collection/mod.ts
+++ server/src/api/collection/mod.ts
@@ -2,3 +2,4 @@ export {default as hi} from './hi';
 export {default as index} from './index';
 export {default as poligon} from './poligon';
 export {default as smile} from './smile';
+export {default as public} from './public';
