afcd0be9e07848b041f9e276c99f1f30028f6ef5
zoodogood <zoodogood@gmail.com>
2023-08-17 14:19:38 +0300

    Add feature: Redirect to /public/index
    If current original url starts with public & status 404

--- server/src/api/collection/404.ts
+++ server/src/api/collection/404.ts
@@ -1,26 +1,40 @@
 
 import type { Request, Response } from "express";
 import { IApiPointModule } from "@/api/interfaces";
+import { MainApiPointsNameEnum } from "@/lib/MainApiPoints";
+
 
 async function use(request: Request, response: Response){
-	const isPublic = await (async () => {
-		const PUBLIC_NAME = "public";
+	
+	const isStatic = await (async () => {
 		const {collection} = await import('@/api');
-		const path = collection.get(PUBLIC_NAME)?.path;
+		const path = collection.get(MainApiPointsNameEnum.Static)?.path;
 		if (!path){
 			return null;
 		}
-		console.dir({path, public: request}, {showHidden: true, depth: 1});
-		
 		
 
 		// to-do: слабое место, плохая, ненаёжная проверка; используется локальная константа PUBLIC_NAME вместо внешней привязки
-		return `/${ request.originalUrl }`.startsWith(path);
+		// Кроме того нарушен принцип единой ответсвенности
+		// important-to-do
+		return `${ request.originalUrl }`.startsWith(path);
 	})();
 
-	const message = isPublic ? "is public 404" : "is 404";
-	response.send(message);
+	if (isStatic){
+		const {collection} = await import('@/api');
+		const get = collection.get(MainApiPointsNameEnum.ClientIndex)?.get;
+		
+		if (!get){
+			response.send("No get")
+			return;
+		}
+		get(request, response);
+		return;
+	}
+	response.send("is 404");
 }
+
+
 
 
 export default {
