9e769e06d05786b8d7de25bd266d47e372bb9581
zoodogood <zoodogood@gmail.com>
2023-08-17 14:08:53 +0300

    Refact

--- server/src/api/collection/mod.ts
+++ server/src/api/collection/mod.ts
@@ -1,5 +1,5 @@
-export {default as hi} from './hi';
-export {default as index} from './index';
-export {default as _404} from './404';
-export {default as smile} from './smile';
-export {default as public} from './public';
+export {default as $1} from './hi';
+export {default as $2} from './index';
+export {default as $3} from './404';
+export {default as $4} from './smile';
+export {default as $5} from './public';
