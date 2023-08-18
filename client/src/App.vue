88c779a46fa53832e32cfd8ff660ddd364e20d6e
Yakyzzza163 <ksport124@gmail.com>
2023-08-18 23:34:36 +0700

    Add code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/zoodogood">Zoodogood</router-link> |
+    <router-link to="/yakyzzza">Yakyzzza</router-link>
   </nav>
   <router-view/>
 </template>
@@ -22,5 +24,10 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+}
+
+nav a:hover {
+  text-decoration: underline;
 }
 </style>
