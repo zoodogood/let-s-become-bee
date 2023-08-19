a07f2479e6f481ca429f8740a68b7218b3b6ca1b
zoodogood <zoodogood@gmail.com>
2023-08-19 13:39:30 +0300

    Create Main

---
+++ client/src/components/layout/Main.vue
@@ -0,0 +1,20 @@
+<template>
+	<main>
+		<slot/>
+	</main>
+</template>
+
+<style scoped>
+	main
+	{
+		animation: apparance 1s;
+		flex-grow: 1;
+		padding-block: 1vh;
+		padding-inline: 5vw;
+		width: 100%;
+	}
+
+	@keyframes apparance {
+		from { opacity: 0; }
+	}
+</style>
