6a609a6c821ac9da72a74e0f8719f01de8e43ecd
zoodogood <zoodogood@gmail.com>
2023-08-19 13:40:37 +0300

    Working on a home page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,3 +1,13 @@
 <template>
-	
+<VLayout>
+	Посмотрим, что можно сделать
+	<br>
+	Здесь будет план
+</VLayout>
+
+
 </template>
+
+<script setup lang = "ts">
+	import VLayout from '@/components/layout/Layout.vue';
+</script>
