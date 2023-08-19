db32a8f2d25a3333ef4db2600e45cedfddc54f3a
zoodogood <zoodogood@gmail.com>
2023-08-19 13:23:31 +0300

    Move header to self component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/zoodogood">Zoodogood</router-link> |
-    <router-link to="/yakyzzza">Yakyzzza</router-link>
-  </nav>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: none;
-}
-
-nav a:hover {
-  text-decoration: underline;
-}
-</style>
