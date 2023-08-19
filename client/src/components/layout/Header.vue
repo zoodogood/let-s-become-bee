5bbc4566d0e6720e92b19f3ab27de1aa3c559bb6
zoodogood <zoodogood@gmail.com>
2023-08-19 13:39:58 +0300

    Init Header

---
+++ client/src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template>
+	<header>
+		<nav>
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link> |
+			<router-link to="/zoodogood">Zoodogood</router-link> |
+			<router-link to="/yakyzzza">Yakyzzza</router-link>
+		</nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
