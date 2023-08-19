b3ae61daa46a541a19afd80d6af235750edc7879
zoodogood <zoodogood@gmail.com>
2023-08-19 13:40:13 +0300

    Create Layout

---
+++ client/src/components/layout/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang = "ts">
+	import VHeader from '@/components/layout/Header.vue';
+	import VMain from '@/components/layout/Main.vue';
+	import VFooter from '@/components/layout/Footer.vue';
+</script>
+
+
+<template>
+	<VHeader/>
+	<VMain>
+		<slot/>
+	</VMain>
+	<VFooter/>
+</template>
