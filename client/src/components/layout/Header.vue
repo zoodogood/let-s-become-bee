a4643408dff0bc4408b3e27a42a65e84432f593f
zoodogood <zoodogood@gmail.com>
2023-08-19 18:28:35 +0300

    Working on a header part 1

--- client/src/components/layout/Header.vue
+++ client/src/components/layout/Header.vue
@@ -1,14 +1,89 @@
+<script setup lang = "ts">
+	import { routes } from '@/router';
+	import VIcon from '@/components/icons/Icon.vue';
+</script>
+
 <template>
 	<header>
-		<nav>
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link> |
-			<router-link to="/zoodogood">Zoodogood</router-link> |
-			<router-link to="/yakyzzza">Yakyzzza</router-link>
-		</nav>
+		
+		<VIcon icon = "home" color = "var( --blue-accent )"/>
   </header>
+
+  <nav class = "navigation-container" style = ";">
+			<li
+				class = "navigation-item"
+				v-for = "{path, name} in routes"
+			>
+				<router-link :to="path" class = "item-link">
+					<span class = "route-name">{{ name }}</span>
+				</router-link>
+			</li>
+		</nav>
+		<span style="font-size: 0.8em; opacity: 0.2;">=</span>
+		{{ `<ДРУЗЬЯ>` }}
 </template>
 
 <style scoped>
 
+	.navigation-container
+	{
+		display: flex;	
+	}
+
+
+	.navigation-container::before
+	{
+		content: '(';
+		margin-right: 0.5em;
+		opacity: 0.2;
+	}
+
+	.navigation-container::after
+	{
+		content: ')';
+		margin-left: 0.5em;
+		opacity: 0.2;
+	}
+
+	.navigation-item:not(:last-child)::after
+	{
+		/* separator */
+		content: '=';
+		margin-inline: 0.5em;
+		opacity: 0.2;
+	}
+
+	.navigation-item
+	{
+		list-style: none;
+		position: relative;
+		display: inline-flex;
+		flex-grow: 1;
+		flex-shrink: 0;
+		
+	}
+
+	.item-link
+	{
+		text-decoration: none;
+	}
+		
+	.route-name
+	{
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	
+
+	.route-name:hover::before
+	{
+		content: '(Нажми меня)';
+		position: absolute;
+		font-size: 0.4em;
+		bottom: -10px;
+		white-space: nowrap;
+	}
+	
 </style>
