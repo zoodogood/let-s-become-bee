31c6870eec34075e470a1ceb8d0649c1efed4cda
zoodogood <zoodogood@gmail.com>
2023-08-23 10:10:29 +0300

    Change cursor

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,3 +2,15 @@
   <router-view/>
 </template>
 
+<style>
+:root
+{
+  --cursor-default: url('@/assets/dot.svg') 16 16, auto;
+	--cursor-pointer: url('@/assets/dots-circle.svg') 8 8, auto;
+}
+
+body
+{
+  cursor: var( --cursor-default );
+}
+</style>
