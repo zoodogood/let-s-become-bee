37e736f2086b71ac5a2077df512893661ab66975
zoodogood <zoodogood@gmail.com>
2023-08-23 10:11:36 +0300

    Add some icons

---
+++ client/src/components/icons/dot.vue
@@ -0,0 +1,10 @@
+
+
+
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class = "icon-svg">
+		<title>circle-small</title>
+		<path d="M12,10A2,2 0 0,0 10,12C10,13.11 10.9,14 12,14C13.11,14 14,13.11 14,12A2,2 0 0,0 12,10Z" />
+	</svg>
+</template>
+
