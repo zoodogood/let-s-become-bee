d4395ed772497e6e6c76f148b88ac1b3f58043da
zoodogood <zoodogood@gmail.com>
2023-08-23 10:11:01 +0300

    Export all icons

--- client/src/components/icons/mod.ts
+++ client/src/components/icons/mod.ts
@@ -1,8 +1,13 @@
 import home from './home.vue';
-
+import create from './create.vue';
+import peopleEmoji from './people-emoji.vue';
+import dot from './dot.vue';
+import dotsCircle from './dots-circle.vue';
 
 const _list = {
-	home
+	home, create, dot,
+	"people-emoji": peopleEmoji,
+	"dots-circle": dotsCircle
 };
 
 const icons = new Map(Object.entries(_list));
